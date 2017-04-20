16bb63ba58785f2da77e4b44a644b2c1d2cdd4c8
Pranay Maharana <pranay.maharana@gmail.com>
2017-04-20 14:48:55 -0400

    --get works in postman

---
+++ LibraryEntityAPI/Controllers/BooksController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using LibraryEntityAPI.DataContext;
+using LibraryEntityAPI.Models;
+using LibraryEntityAPI.Services;
+
+namespace LibraryEntityAPI.Controllers
+{
+    public class BooksController : ApiController
+    {
+        private LibraryContext db = new LibraryContext();
+
+        // GET: api/Books
+        public IEnumerable<Books> GetBooks()
+        {
+            return new BookServices().GetAllBooks();
+        }
+
+        // GET: api/Books/5
+        [ResponseType(typeof(Books))]
+        public IHttpActionResult GetBooks(int id)
+        {
+            Books books = db.Books.Find(id);
+            if (books == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(books);
+        }
+
+        // PUT: api/Books/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutBooks(int id, Books books)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != books.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(books).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BooksExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Books
+        [ResponseType(typeof(Books))]
+        public IHttpActionResult PostBooks(Books books)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Books.Add(books);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = books.Id }, books);
+        }
+
+        // DELETE: api/Books/5
+        [ResponseType(typeof(Books))]
+        public IHttpActionResult DeleteBooks(int id)
+        {
+            Books books = db.Books.Find(id);
+            if (books == null)
+            {
+                return NotFound();
+            }
+
+            db.Books.Remove(books);
+            db.SaveChanges();
+
+            return Ok(books);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BooksExists(int id)
+        {
+            return db.Books.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ LibraryEntityAPI/Services/BookServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LibraryEntityAPI.Models;
+using LibraryEntityAPI.DataContext;
+
+namespace LibraryEntityAPI.Services
+{
+    public class BookServices
+    {
+        LibraryContext db = new LibraryContext();
+
+        public IEnumerable<Books> GetAllBooks()
+        {
+            var list = db.Books.ToList();
+            return list;
+        }
+
+    }
+}
