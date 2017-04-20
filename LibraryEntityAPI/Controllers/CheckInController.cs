7efaf321d49d9d0830eefd3e5520fd6a1eeabff9
Pranay Maharana <pranay.maharana@gmail.com>
2017-04-20 18:19:51 -0400

    --checkin/out done

---
+++ LibraryEntityAPI/Controllers/CheckInController.cs
@@ -0,0 +1,51 @@
+﻿using LibraryEntityAPI.DataContext;
+using LibraryEntityAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace LibraryEntityAPI.Controllers
+{
+    public class CheckInController : ApiController
+    {
+        private LibraryContext db = new LibraryContext();
+
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutBooks(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Books books = db.Books.Find(id);
+            if (books == null)
+            {
+                return NotFound();
+            }
+
+            var updating = db.Books.FirstOrDefault(f => f.Id == id);
+            if (updating.IsCheckedOut == true)
+            {
+
+                updating.IsCheckedOut = false;
+                db.SaveChanges();
+                return Ok(new { Message = "Thank you for bringing the book back homes" });
+            }
+
+
+            else
+            {
+                return Ok(new
+                {
+                    Message = "The book is already checked in "
+                });
+            }
+
+        }
+        }
+}
---
+++ LibraryEntityAPI/Controllers/CheckOutController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using LibraryEntityAPI.DataContext;
+using LibraryEntityAPI.Models;
+
+namespace LibraryEntityAPI.Controllers
+{
+    public class CheckOutController : ApiController
+    {
+        private LibraryContext db = new LibraryContext();
+
+
+        // PUT: api/CheckOut/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutBooks(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Books books = db.Books.Find(id);
+            if (books == null)
+            {
+                return NotFound();
+            }
+
+            var updating = db.Books.FirstOrDefault(f => f.Id == id);
+            if (!updating.IsCheckedOut.GetValueOrDefault())
+            {
+
+                updating.IsCheckedOut = true;
+                updating.LastCheckedOutDate = DateTime.Now;
+                updating.DueBackDate = DateTime.Now.AddDays(10);
+                db.SaveChanges();
+                return Ok(updating);
+            }
+            else
+            {
+                return Ok(new
+                {
+                    Message = "The book is already checked out. It will be due back on ", books.DueBackDate
+                });
+            }
+
+
+            //try
+            //{
+            //}
+            //catch (DbUpdateConcurrencyException)
+            //{
+            //    if (!BooksExists(id))
+            //    {
+            //        return NotFound();
+            //    }
+            //    else
+            //    {
+            //        throw;
+            //    }
+            //}
+
+            //return StatusCode(HttpStatusCode.NoContent);
+        }
+
+       
+    }
+}
